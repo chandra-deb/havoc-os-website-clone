e6108e156fed7896b6abfad661f4b41298ef6a75
chandra-deb <mail.chandradeb@gmail.com>
2021-09-08 20:37:25 +0600

    gondogol

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-let xBtn=document.querySelector(".x-btn"),targetTag=document.querySelector(".m-nav");xBtn.addEventListener("click",(function(){targetTag.style.opacity="0",targetTag.style.visibility="hidden"}));let mLink=document.querySelectorAll(".m-link");mLink.forEach((function(e){e.addEventListener("click",(function(){targetTag.style.opacity="0",targetTag.style.visibility="hidden"}))}));let hamBtn=document.querySelector("#ham-burger");hamBtn.addEventListener("click",(function(){targetTag.style.opacity="1",targetTag.style.visibility="visible"}));let time=document.querySelector("#time"),day=document.querySelector("#day");time.innerHTML=(new Date).toTimeString().slice(0,5);let weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];day.innerHTML=weekday[(new Date).getUTCDay()];let loader=document.querySelector("#loader"),mainContainer=document.querySelector("#main-container");window.addEventListener("load",(function(){setTimeout((function(){loader.style.height=0,loader.style.opacity=0,loader.style.visibility="hidden",mainContainer.style.opacity="1"}),500)}));var swiper=new Swiper(".mySwiper",{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",dragSize:10,initialSlide:4,autoHeight:!0,coverflowEffect:{rotate:0,modifier:8,slideShadows:!0},pagination:{el:".swiper-pagination",clickable:!0}});swiper.on("slideChange",(function(){}));let slide1=document.querySelector("#s-1"),slide2=document.querySelector("#s-2"),slide3=document.querySelector("#s-3"),slide4=document.querySelector("#s-4"),slide5=document.querySelector("#s-5"),slide6=document.querySelector("#s-6"),slide7=document.querySelector("#s-7"),slide8=document.querySelector("#s-8"),slide9=document.querySelector("#s-9"),slide10=document.querySelector("#s-10");slide1.addEventListener("click",(function(){swiper.slideTo(0)})),slide2.addEventListener("click",(function(){swiper.slideTo(1)})),slide3.addEventListener("click",(function(){swiper.slideTo(2)})),slide4.addEventListener("click",(function(){swiper.slideTo(3)})),slide5.addEventListener("click",(function(){swiper.slideTo(4)})),slide6.addEventListener("click",(function(){swiper.slideTo(5)})),slide7.addEventListener("click",(function(){swiper.slideTo(6)})),slide8.addEventListener("click",(function(){swiper.slideTo(7)})),slide9.addEventListener("click",(function(){swiper.slideTo(8)})),slide10.addEventListener("click",(function(){swiper.slideTo(9)}));
+let xBtn = document.querySelector(".x-btn"),
+  targetTag = document.querySelector(".m-nav");
+xBtn.addEventListener("click", function () {
+  (targetTag.style.opacity = "0"), (targetTag.style.visibility = "hidden");
+});
+let mLink = document.querySelectorAll(".m-link");
+mLink.forEach(function (e) {
+  e.addEventListener("click", function () {
+    (targetTag.style.opacity = "0"), (targetTag.style.visibility = "hidden");
+  });
+});
+let hamBtn = document.querySelector("#ham-burger");
+hamBtn.addEventListener("click", function () {
+  (targetTag.style.opacity = "1"), (targetTag.style.visibility = "visible");
+});
+let time = document.querySelector("#time"),
+  day = document.querySelector("#day");
+time.innerHTML = new Date().toTimeString().slice(0, 5);
+let weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+day.innerHTML = weekday[new Date().getUTCDay()];
+let loader = document.querySelector("#loader"),
+  mainContainer = document.querySelector("#main-container");
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    (loader.style.height = 0),
+      (loader.style.opacity = 0),
+      (loader.style.visibility = "hidden"),
+      (mainContainer.style.opacity = "1");
+  }, 500);
+});
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: !0,
+  centeredSlides: !0,
+  loop: !0,
+  loopSlides: 2,
+  loopAdditionalSlides: 2,
+  slidesPerView: "2",
+  slideToClickedSlide: !0,
+  dragSize: 10,
+  autoHeight: !0,
+  coverflowEffect: { rotate: 0, depth: 80, modifier: 7.5, slideShadows: !0 },
+  pagination: { el: ".swiper-pagination", clickable: !0 },
+});
--- src/main.js
+++ src/main.js
@@ -1,114 +1,76 @@
-let xBtn = document.querySelector('.x-btn');
-let targetTag = document.querySelector('.m-nav');
-xBtn.addEventListener('click', function () {
-    targetTag.style.opacity = '0';
-    targetTag.style.visibility = 'hidden';
+let xBtn = document.querySelector(".x-btn");
+let targetTag = document.querySelector(".m-nav");
+xBtn.addEventListener("click", function () {
+  targetTag.style.opacity = "0";
+  targetTag.style.visibility = "hidden";
 });
 
-let mLink = document.querySelectorAll('.m-link');
+let mLink = document.querySelectorAll(".m-link");
 mLink.forEach(function (item) {
-    item.addEventListener('click', function () {
-        targetTag.style.opacity = '0';
-        targetTag.style.visibility = 'hidden';
-    });
+  item.addEventListener("click", function () {
+    targetTag.style.opacity = "0";
+    targetTag.style.visibility = "hidden";
+  });
 });
 
-let hamBtn = document.querySelector('#ham-burger');
-hamBtn.addEventListener('click', function () {
-    // targetTag.classList.toggle('d-none');
-    targetTag.style.opacity = '1';
-    targetTag.style.visibility = 'visible';
+let hamBtn = document.querySelector("#ham-burger");
+hamBtn.addEventListener("click", function () {
+  // targetTag.classList.toggle('d-none');
+  targetTag.style.opacity = "1";
+  targetTag.style.visibility = "visible";
 });
 
-let time = document.querySelector('#time');
-let day = document.querySelector('#day');
+let time = document.querySelector("#time");
+let day = document.querySelector("#day");
 time.innerHTML = new Date().toTimeString().slice(0, 5);
-let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 day.innerHTML = weekday[new Date().getUTCDay()];
-
 
 // Preloader
 
-let loader = document.querySelector('#loader');
+let loader = document.querySelector("#loader");
 let mainContainer = document.querySelector("#main-container");
-window.addEventListener('load', function () {
-    setTimeout(function () {
-        loader.style.height = 0;
-        loader.style.opacity = 0;
-        loader.style.visibility = 'hidden';
-        mainContainer.style.opacity = '1';
-    }, 500);
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    loader.style.height = 0;
+    loader.style.opacity = 0;
+    loader.style.visibility = "hidden";
+    mainContainer.style.opacity = "1";
+  }, 500);
 });
 
-
-
-
 // Swiper Settings
-        var swiper = new Swiper(".mySwiper", {
-            effect: "coverflow",
-            grabCursor: true,
-            centeredSlides: true,
-            slidesPerView: "auto",
-            dragSize: 10,
-            // autoplay: true,
-            initialSlide: 4,
-            autoHeight: true,
-            coverflowEffect: {
-                rotate: 0,
-                // stretch: 0,
-                // depth: 200,
-                modifier: 8,
-                slideShadows: true,
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                // dynamicBullets: true,
-                clickable:true,
-            },
-        });
-        
-        swiper.on('slideChange', function () {
-            console.log(swiper.activeIndex);
-        });
-
-        let slide1 = document.querySelector('#s-1');
-        let slide2 = document.querySelector('#s-2');
-        let slide3 = document.querySelector('#s-3');
-        let slide4 = document.querySelector('#s-4');
-        let slide5 = document.querySelector('#s-5');
-        let slide6 = document.querySelector('#s-6');
-        let slide7 = document.querySelector('#s-7');
-        let slide8 = document.querySelector('#s-8');
-        let slide9 = document.querySelector('#s-9');
-        let slide10 = document.querySelector('#s-10');
-
-        slide1.addEventListener('click', function () {
-            swiper.slideTo(0);
-        });
-        slide2.addEventListener('click', function () {
-            swiper.slideTo(1);
-        });
-        slide3.addEventListener('click', function () {
-            swiper.slideTo(2);
-        });
-        slide4.addEventListener('click', function () {
-            swiper.slideTo(3);
-        });
-        slide5.addEventListener('click', function () {
-            swiper.slideTo(4);
-        });
-        slide6.addEventListener('click', function () {
-            swiper.slideTo(5);
-        });
-        slide7.addEventListener('click', function () {
-            swiper.slideTo(6);
-        });
-        slide8.addEventListener('click', function () {
-            swiper.slideTo(7);
-        });
-        slide9.addEventListener('click', function () {
-            swiper.slideTo(8);
-        });
-        slide10.addEventListener('click', function () {
-            swiper.slideTo(9);
-        });
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  loopSlides: 2,
+  loopAdditionalSlides: 2,
+  slidesPerView: "2",
+  slideToClickedSlide: true,
+  dragSize: 10,
+  // autoplay: true,
+  // initialSlide: 4,
+  autoHeight: true,
+  coverflowEffect: {
+    rotate: 0,
+    // stretch: 0,
+    depth: 80,
+    modifier: 7.5,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    // dynamicBullets: true,
+    clickable: true,
+  },
+});
